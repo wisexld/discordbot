a4f14bf6c857ce2d90e08f7b28f80c6bd7eba16d
Wxld-dev <dev.wxld@protonmail.com>
2022-08-24 22:30:22 -0300

    Commands Handler

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const { token } = require('./config.json')
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+require("./Handlers/Events")(client);
 
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
-});
+// client.on('interactionCreate', async interaction => {
+//   if (!interaction.isCommand()) return;
+
+//   if (interaction.commandName === 'ping') {
+//     await interaction.reply('Pong!');
+//   }
+// });
 
 client.login(token);

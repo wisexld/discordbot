9e5be5a7fcd5e019223d320b66b58473cabe5349
Wxld-dev <dev.wxld@protonmail.com>
2022-08-24 22:30:22 -0300

    Commands Handler

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const { token } = require('./config.json')
 
+require("./Handlers/Events")(client);
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
